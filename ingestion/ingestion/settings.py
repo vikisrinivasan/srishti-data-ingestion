ed6755c462900f17e7882e5abdd60d120fc123dc
vignesh srinivasan <ganeshs.srinivasan@gmail.com>
2022-03-12 11:53:13 +0800

    updated settings

--- ingestion/ingestion/settings.py
+++ ingestion/ingestion/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'formtools',
     'ingestion',
     'django_tables2'
 ]
